fc7e57e3a57c86b91e69502398380a9da1f5f1f5
AlaaSulimankhazaleh <alaak1993.ak@gmail.com>
2022-11-23 09:55:25 +0300

    Add To Check Defferent Port

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private httpHeaders = new HttpHeaders().set('Accept', 'application/json').set('Access-Control-Allow-Origin', '*').set('content-type', 'application/json; charset=utf-8');
   title = 'helpdesk';
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getValues().subscribe(x => {
+      console.log(x);
+    })
+  }
+  getValues(): Observable<any> {
+    return this.http.get<any>("http://localhost:5006/api/values",
+      { headers: this.httpHeaders, responseType: 'json' });
+  }
+
 }
